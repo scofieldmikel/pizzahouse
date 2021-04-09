07d352be5eaec93126d0f94a2783b845f973ed57
scofield <shoyemimichealiyonu@gmail.com>
2021-04-09 16:12:19 +0100

    Customize auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     You are logged in!
+                    <p><a href="/pizzas">View all Pizza Orders</a></p>
                 </div>
             </div>
         </div>
