029fdbc00c19ae33915ed61b08026431cf578ef2
scofield <shoyemimichealiyonu@gmail.com>
2021-04-09 15:59:54 +0100

    Auth setup

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::post('/pizzas', 'PizzaController@store');
 Route::get('/pizzas/{id}', 'PizzaController@show');
 Route::delete('/pizzas/{id}', 'PizzaController@destroy');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
